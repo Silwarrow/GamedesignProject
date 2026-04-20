7b4af164cd9fb1de190c3ae20d20ed9cfdb5b31b
jf-246457 <jf-246457@rwu.de>
2026-04-20 08:59:38 +0200

    adds size regulated ease in speed

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CharacterControler : MonoBehaviour
 {
     public float speed = 5f;
+    public float accelerationValue = 1f;
     public bool devOut = false;
+    private int time = 0;
     private int movementDuration = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
@@ -21,12 +23,22 @@ public class CharacterControler : MonoBehaviour
         //Nach bewegungsdauer
         if(movement == UnityEngine.Vector3.zero){
             movementDuration = 0;
+            time = 0;
         }else{
             movementDuration++;
         }
-        float acceleration = 1 - Mathf.Exp(-movementDuration / 200f);
+        float size = (transform.localScale.x+transform.localScale.y+transform.localScale.z)/3f;
+        float acceleration = 1 - Mathf.Exp(-movementDuration * accelerationValue/(size*3.14159f));
         if(devOut){
             Debug.Log(acceleration);
+            
+            if(movementDuration > 0 && acceleration == 1)
+            {
+                if(time == 0){
+                    time = movementDuration;
+                }
+                Debug.Log("Maximalgeschwindigkeit erreicht bei time = " + time);
+            }
         }
 
         //bewegen
