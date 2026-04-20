e22491a85a75e7328b11d80c557624ce3e628893
jf-246457 <jf-246457@rwu.de>
2026-04-20 10:16:17 +0200

    adds ease out and clean up code

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -7,8 +7,9 @@ public class CharacterControler : MonoBehaviour
     public float speed = 5f;
     public float accelerationValue = 1f;
     public bool devOut = false;
-    private int time = 0;
-    private int movementDuration = 0;
+
+    private float acceleration = 0f, accelTime = 0, deccelTime = 0;
+    private UnityEngine.Vector3 momentum = UnityEngine.Vector3.zero;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         
@@ -16,33 +17,36 @@ public class CharacterControler : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+        float size = (transform.localScale.x+transform.localScale.y+transform.localScale.z)/3f;
+        
         //Bewegungsrichtung berechnen
         UnityEngine.Vector3 movement = new UnityEngine.Vector3( toInt(Input.GetKey(KeyCode.D)) - toInt(Input.GetKey(KeyCode.A)), 0, 
                                                                 toInt(Input.GetKey(KeyCode.W)) - toInt(Input.GetKey(KeyCode.S)));
 
         //Nach bewegungsdauer
-        if(movement == UnityEngine.Vector3.zero){
-            movementDuration = 0;
-            time = 0;
-        }else{
-            movementDuration++;
+        if(movement == UnityEngine.Vector3.zero)
+        {
+            deccelTime+=Time.deltaTime;
+            accelTime=0;
+            acceleration = Mathf.Exp(-deccelTime* (accelerationValue/size));
         }
-        float size = (transform.localScale.x+transform.localScale.y+transform.localScale.z)/3f;
-        float acceleration = 1 - Mathf.Exp(-movementDuration * accelerationValue/(size*3.14159f));
-        if(devOut){
-            Debug.Log(acceleration);
-            
-            if(movementDuration > 0 && acceleration == 1)
-            {
-                if(time == 0){
-                    time = movementDuration;
-                }
-                Debug.Log("Maximalgeschwindigkeit erreicht bei time = " + time);
-            }
+        else
+        {
+            momentum = movement;
+            accelTime+=Time.deltaTime;
+            deccelTime=0;
+            acceleration = 1 -Mathf.Exp(-accelTime*(accelerationValue/size));
         }
+        
 
         //bewegen
-        transform.Translate(movement * speed * acceleration * Time.deltaTime);
+        transform.Translate(momentum * speed * acceleration * Time.deltaTime);
+
+        //Dev output
+        if (devOut)
+        {
+            Debug.Log("Momentum: " + momentum + " Acceleration: " + acceleration);
+        }
     }
 
     private int toInt(bool b){
