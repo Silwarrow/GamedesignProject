60cc760f5cecffd30adf71e65d5247bffbbc6063
Datengeist <janikfo04@gmail.com>
2026-04-24 19:22:14 +0200

    Add Spawnpoints in SampleScene

--- Assets/Scripts/Spawnpoint.cs
+++ Assets/Scripts/Spawnpoint.cs
@@ -4,6 +4,8 @@ public class Spawnpoint : MonoBehaviour
 {
 
     public UnityEngine.Vector3 spawnPoint = new UnityEngine.Vector3(0, 1, 0);
+    public UnityEngine.Quaternion spawnRotation = new UnityEngine.Quaternion(0, 0, 0, 1);
+    public UnityEngine.Vector3 spawnScale = new UnityEngine.Vector3(1, 1, 1);
 
     private GameObject playerManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +19,8 @@ public class Spawnpoint : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerManager.GetComponent<RespawnControler>().respawnPoint = spawnPoint;
+            playerManager.GetComponent<RespawnControler>().respawnRotation = spawnRotation;
+            playerManager.GetComponent<RespawnControler>().respawnScale = spawnScale;
         }
     }
 }
