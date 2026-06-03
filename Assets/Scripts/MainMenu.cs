57dabb5e462ca8a49706bb7ec537f81d550b2e2b
Silwarrow <robin.logar@icloud.com>
2026-06-03 14:18:50 +0200

    Main menu leads to Level 1

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,12 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(2);
+        // 0 = Main Menu
+        // 1 = SampleScene
+        // 2 = Level 1
+        // 3 = Level 2
+        // 4 = Level 3
     }
 
     public void QuitGame()
