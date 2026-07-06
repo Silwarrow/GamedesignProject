2c7b80de6a54a0f9cb0923a15921661caead9905
Datengeist <janikfo04@gmail.com>
2026-07-06 22:40:27 +0200

    make it buildable

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,7 +13,11 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         buttonClickSound.Play();
-        UnityEditor.EditorApplication.isPlaying = false;
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
     }
     
     public void PlayButtonSound()
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -57,7 +57,11 @@ public class PauseMenu : MonoBehaviour
     public void QuitGame()
     {
         buttonClickSound.Play();
-        UnityEditor.EditorApplication.isPlaying = false;
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
     }
 
     public void PlayButtonSound()
